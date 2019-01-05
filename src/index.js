1435f55ed4f06e1cfed48944471b962a975bcd65
Lasse <lassetange@gmail.com>
2019-01-05 15:19:00 +0100

    Use Hashrouter to make it function with gh-pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import GlobalStyles from './utils/GlobalStyles';
 import Home from './containers/Home';
 import Posts from './containers/Posts';
@@ -10,12 +10,12 @@ import * as serviceWorker from './utils/serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
+    <HashRouter>
       <Switch>
         <Route path="/r/:id" component={Posts} />
         <Route path="*" component={Home} />
       </Switch>
-    </Router>
+    </HashRouter>
     <GlobalStyles />
   </Provider>,
   document.getElementById('root'),
