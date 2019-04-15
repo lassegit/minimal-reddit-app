0e7bd6028d01705fb6f4a3b4aefbb841d4f26feb
Lasse <lassetange@gmail.com>
2019-04-15 15:01:44 +0200

    Functional components

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -8,35 +8,31 @@ import SubAdd from '../SubAdd';
 import SubList from '../SubList';
 import { H1 } from '../../components/Heading';
 
-export class Home extends React.Component {
-  render() {
-    const { sub } = this.props;
-
-    if (sub.length) {
-      const randomSub = sub[Math.floor(Math.random() * sub.length)];
-      return <Redirect push to={`/r/${randomSub}`} />;
-    }
-
-    return (
-      <React.Fragment>
-        <Layout
-          column1={
-            <React.Fragment>
-              <SubAdd /> <SubList />
-            </React.Fragment>
-          }
-          column2={
-            <React.Fragment>
-              <H1>Casually read the most popular posts on selected subreddits</H1>
-              <P>Only posts above a certain popularity are shown and then sorted by date.</P>
-              <P>Visited links are slighty darker.</P>
-            </React.Fragment>
-          }
-        />
-      </React.Fragment>
-    );
+const Home = ({ sub }) => {
+  if (sub.length) {
+    const randomSub = sub[Math.floor(Math.random() * sub.length)];
+    return <Redirect push to={`/r/${randomSub}`} />;
   }
-}
+
+  return (
+    <>
+      <Layout
+        column1={
+          <>
+            <SubAdd /> <SubList />
+          </>
+        }
+        column2={
+          <>
+            <H1>Casually read the most popular posts on selected subreddits</H1>
+            <P>Only posts above a certain popularity are shown and then sorted by date.</P>
+            <P>Visited links are slighty darker.</P>
+          </>
+        }
+      />
+    </>
+  );
+};
 
 Home.propTypes = {
   sub: PropTypes.arrayOf(PropTypes.string).isRequired,
